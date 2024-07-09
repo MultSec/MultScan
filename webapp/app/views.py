782232f9162de0a2d919513dd88764cfee958b62
pengrey <rodrigoflima@ua.pt>
2024-07-09 12:14:46 +0100

    Added upload and fileinfo

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,5 +1,8 @@
 from flask import render_template, request, redirect, jsonify, send_file
-from app import app
+from app import app, Log
+import os
+import shutil
+from utils.utils import getFileInfo
 
 # Route for favicon
 @app.route('/favicon.ico')
@@ -16,6 +19,42 @@ def index():
 @app.route('/api/v1/machines', methods=['GET'])
 def plugins():
     return app.config['config']['machines']
+
+# Upload sample for a given id
+@app.route('/api/v1/sample/upload/<id>', methods=['POST'])
+def upload(id):
+    Log.info(f"[\033[34m{id}\033[0m] Saving sample")
+
+    # Check if 'sample' file is in the request
+    if 'sample' not in request.files:
+        Log.error("No sample file part in the request")
+        return jsonify({"error": "No sample file part"}), 400
+
+    # Get the file from the request
+    file = request.files['sample']
+
+    # Make directory
+    Log.info(f"[\033[34m{id}\033[0m] Generating temp dir")
+    os.makedirs(f'./uploads/{id}')
+    
+    # Save the file to the uploads folder as sample
+    file.save(f'./uploads/{id}/sample')
+
+    # Return the success message
+    return jsonify({"message": "File uploaded successfully"})
+
+# Retrieve fileinfo for a given id sample
+@app.route('/api/v1/sample/fileinfo/<id>', methods=['GET'])
+def getResult(id):
+
+    # Retrieve fileinfo
+    fileInfo = getFileInfo(f'./uploads/{id}/sample')
+
+    # Remove directory
+    Log.info(f"[\033[34m{id}\033[0m] Removing temp dir")
+    shutil.rmtree(f'./uploads/{id}')
+
+    return fileInfo
 
 # Route for errors
 @app.errorhandler(404)
---
+++ webapp/utils/utils.py
@@ -0,0 +1,58 @@
+import magic
+import hashlib
+import os
+import requests
+
+def getFileInfo(filename):
+    # Create emptyt dictionary to store file info
+    fileInfo = {}
+    fileInfo['info'] = {}
+            
+    # Get file size in bytes and convert to MB
+    sizeBytes = os.path.getsize(filename)
+    sizeMB = sizeBytes / (1024 * 1024)
+    fileInfo['info']['size'] = f'{sizeMB:.2f} MB ({sizeBytes} bytes)'
+
+    # Get file type
+    fileInfo['info']['type'] = magic.from_file(filename)
+
+    # Get digests
+    fileInfo['info']['digests'] = []
+    
+    with open(filename, 'rb') as f:
+        data = f.read()
+        fileInfo['info']['digests'].append("MD5:" + hashlib.md5(data).hexdigest())
+        fileInfo['info']['digests'].append("SHA-1:" + hashlib.sha1(data).hexdigest())
+        fileInfo['info']['digests'].append("SHA-256:" + hashlib.sha256(data).hexdigest())
+
+    # Public presence
+    digest = fileInfo['info']['digests'][2].split(':')[1]
+    fileInfo['info']['public_presence'] = {}
+    fileInfo['info']['public_presence']['Virustotal'] = checkVirusTotal(digest)
+
+    # Return fileInfo
+    return fileInfo
+
+def checkVirusTotal(hash):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0',
+        'Accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Referer': 'https://www.virustotal.com/',
+        'Content-Type': 'application/json',
+        'X-Tool': 'vt-ui-main',
+        'X-App-Version': 'v1x249x0',
+        'Accept-Ianguage': 'en-US,en;q=0.9,es;q=0.8',
+        'X-Vt-Anti-Abuse-Header': 'a',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Te': 'trailers',
+    }
+
+    response = requests.get('https://www.virustotal.com/ui/files/' + hash, headers=headers)
+
+    # Check for response {"error":{"code":"NotFoundError","message":"Resource not found."}} that indicates file not found in VirusTotal
+
+    return not response.json().get('error')
